397fd5cfc53eae944afaa3d483e7b9ae182c19c7
PathanUmar <rehankhan12716@gmail.com>
2026-09-04 12:28:32 +0530

    add some fixex

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Layout from "./pages/Layout";
 import { Toaster } from "./components/ui/sonner";
-import Routess from "./api/cron/check-prices/route";
 
 function App() {
   return (
@@ -9,7 +8,6 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Layout />} />
-          <Route path="/api/cron/check-prices" element={<Routess />} />
         </Routes>
       </BrowserRouter>
       <Toaster />
--- src/api/cron/check-prices/route.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-function Routess() {
-  const hjhf = { kef: "lkjgjkdg;kd" };
-  console.log(hjhf.kef);
-}
-
-export default Routess;
